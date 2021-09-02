b9ec39ee62aa917f34bb3bee27b5fe47e37af97c
ViniciusJrCarlos <vinicius.jcarlos@hotmail.com>
2021-09-02 02:09:54 -0300

    arquivos do curso angular atualizados

---
+++ primeiroprojeto/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeuPrimeiroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ primeiroprojeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+
+        <p>Meu primeiro component com Angular </p>
+    
+    `
+})
+
+export class MeuPrimeiroComponent{}
