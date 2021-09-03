d71346bac1352f9d795cf7cbf9422b3f84e7910c
ViniciusJrCarlos <vinicius.jcarlos@hotmail.com>
2021-09-03 01:06:31 -0300

    arquivos atualizados 03/09

--- primeiroprojeto/src/app/app.module.ts
+++ primeiroprojeto/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MeuPrimeiroComponent
+    MeuPrimeiroComponent,
+    MeuPrimeiro2Component
   ],
   imports: [
     BrowserModule,
---
+++ primeiroprojeto/src/app/cursos/cursos.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CursosComponent } from './cursos.component';
+
+
+
+@NgModule({
+  declarations: [
+    CursosComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CursosModule { }
