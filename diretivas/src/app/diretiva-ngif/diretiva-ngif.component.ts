b471286cf7bf94935004988deafcaeb976dd9cf3
ViniciusJrCarlos <vinicius.jcarlos@hotmail.com>
2022-01-11 00:15:40 -0300

    aula - 20 - angular CLI ng lint, ng test

---
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngif',
+  templateUrl: './diretiva-ngif.component.html',
+  styleUrls: ['./diretiva-ngif.component.scss']
+})
+export class DiretivaNgifComponent implements OnInit {
+
+  //minhavarivel : string;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
